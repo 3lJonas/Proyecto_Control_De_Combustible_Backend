e8c9227ba47b208ceefdd5ae60fbddb4f98fed0e
Reclax <marcoserrano2206@gmail.com>
2025-11-06 15:05:54 -0500

    Se agrega entidades y contexto de base de datos

---
+++ MicroservicioRutas/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MicroservicioRutas.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MicroservicioRutas.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+        }
+
+        public DbSet<Ruta> Rutas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Asinacion de ids 
+            modelBuilder.Entity<Ruta>().Property(u => u.estado).HasDefaultValue(Estado.Activo);
+            modelBuilder.Entity<Ruta>().HasKey(c => c.id);
+        }
+    }
+}
